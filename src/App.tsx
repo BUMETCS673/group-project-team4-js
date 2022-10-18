a59d1731944055fa9aaef1fefc875eb876efb30a
Ryan Burns <mr.ryan.burns@outlook.com>
2022-10-17 22:44:35 -0400

    🦄 refactor(custom): made some changes to Search Bar
    I added curved edges and a placeholder to the searchbar

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,6 @@ function App() {
 
     return (
         <div className="App">
-            <img src={logo} />
             <SearchBar></SearchBar>
             <Intro></Intro>
             <WelcomePage></WelcomePage>
